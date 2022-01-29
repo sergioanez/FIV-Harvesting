43b7b6d4dd0fa8f80d31e1272ff6b175b4b6bf75
Phillip <33733645+PhillipMilner@users.noreply.github.com>
2022-01-29 15:10:25 -0700

    Added encoder output and single rotation of motor

--- actuator_system_firmware/actuator_system_firmware.ino
+++ actuator_system_firmware/actuator_system_firmware.ino
@@ -16,17 +16,20 @@ int xAccel = 200;
 int ySpeed = 40000;
 int yAccel = 200;
 
+long xA_pos  = 1;
+long new_xA_pos;
 
 void setup() {
   xA_axis.setMaxSpeed(xSpd);
   xA_axis.setAcceleration(xAccel);
 
-  Serial.begin(9600);  // Start Serial
+Serial.begin(9600);  // Start Serial
+Serial.println("Encoder Test:");
 }
 
 void loop() {
-  xAxisForward(4000);
-  xAxisBackward(-4000);
+  xAxisForward(3200);
+  xAxisBackward(-3200);
 }
 
 
@@ -39,6 +42,13 @@ int xAxisForward (int dist) {
   xA_axis.move(dist);
   while (xA_axis.distanceToGo() > 0) {
     xA_axis.run();
+    new_xA_pos = xA_encoder.read() *0.09;
+    if (new_xA_pos != xA_pos) {
+      Serial.print("count = ");
+      Serial.print(new_xA_pos);
+      Serial.println();
+      xA_pos = new_xA_pos;
+    }
   }
 }
 
@@ -48,5 +58,12 @@ int xAxisBackward (int dist) {
   xA_axis.move(dist);
   while (xA_axis.distanceToGo() < 0) {
     xA_axis.run();
+    new_xA_pos = xA_encoder.read() * 0.09;
+    if (new_xA_pos != xA_pos) {
+      Serial.print("count = ");
+      Serial.print(new_xA_pos);
+      Serial.println();
+      xA_pos = new_xA_pos;
+    }
   }
 }
