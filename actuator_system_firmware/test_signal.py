2aafb5240169396860982a4e342bfa8938323418
Phillip <33733645+PhillipMilner@users.noreply.github.com>
2022-03-09 16:25:00 -0700

    updates to python

--- actuator_system_firmware/test_signal.py
+++ actuator_system_firmware/test_signal.py
@@ -3,16 +3,19 @@ import serial
 import time 
 i = 0;
 
-ser = serial.Serial('/dev/cu.usbmodem104945401')  # open serial port
-print(ser.name)         # check which port was really used
+serialControl = serial.Serial('/dev/cu.usbmodem104945401')  # open serial port
+serialReadEncoder = serial.Serial('/dev/tty.usbmodem103309801')  # open serial port
+print(serialControl.name)         # check which port was really used
 
 while True:
     x = round(100*math.sin(i*0.1),5) # in radians
     y = round(100*math.sin(i*0.1),5) # in radians
     command  = '<X' + str(x) +'Y'+str(y) + '>' + '\n'
-    ser.write(command.encode())     # write a string #<X25Y40>
+    serialControl.write(command.encode())     # write a string #<X25Y40>
     print(command.encode())
     i += 1; 
     time.sleep(0.1)
+    encoderData = serialReadEncoder.read() 
     
-ser.close()
+serialControl.close()
+serialReadEncoder.close()
