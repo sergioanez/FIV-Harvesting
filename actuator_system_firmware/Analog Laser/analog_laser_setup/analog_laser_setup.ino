35a4c9da7c42fa0e561be7fc223dda6d094d0d9a
Phillip <33733645+PhillipMilner@users.noreply.github.com>
2022-03-09 17:38:47 -0700

    update for laser

--- actuator_system_firmware/Analog Laser/analog_laser_setup/analog_laser_setup.ino
+++ actuator_system_firmware/Analog Laser/analog_laser_setup/analog_laser_setup.ino
@@ -1,17 +1,19 @@
 
 float input_signal = 0;
 float calibration = 1;
-
+float distance = 0;
 void setup() {
   pinMode(2, INPUT);
-
+// 81.0 for 50 mm
+// 314.0 for 80 mm
 Serial.begin(9600);
 }
 
 void loop() {
   
 input_signal = analogRead(A0);
-distance = input_signal * calibration;
-serial.println(distance)
+distance = 50 + (input_signal-81) * 0.129;
+Serial.println(distance);
+delay(1000);
 
 }
