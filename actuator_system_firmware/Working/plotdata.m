723d26911862e471cc91f81e60b1395ae98efc91
Phillip <33733645+PhillipMilner@users.noreply.github.com>
2022-03-23 10:54:50 -0600

    update

--- actuator_system_firmware/Working/plotdata.m
+++ actuator_system_firmware/Working/plotdata.m
@@ -3,8 +3,8 @@ clc
 close all
 w = 0.1;
 encoder_raw2 = [];
-input_raw = cell2mat(table2cell(readtable('input_test_14.csv')));
-encoder_raw = cell2mat(table2cell(readtable('encoder_57.csv',detectImportOptions('fin.csv'))));
+input_raw = cell2mat(table2cell(readtable('input_test_0.csv')));
+encoder_raw = cell2mat(table2cell(readtable('encoder_0.csv',detectImportOptions('encoder_0.csv'))));
 
 t_shift = input_raw(1,1);
 
@@ -14,7 +14,7 @@ plot(input_raw(:,1)-t_shift, input_raw(:,4));
 
 
 
-% figure 
-% plot(encoder_raw(:,1), encoder_raw(:,4), input_raw(:,1), input_raw(:,2));
-% 
-% legend({'Enc','In'})
+figure 
+plot(encoder_raw(:,1), encoder_raw(:,5), input_raw(:,1), input_raw(:,2));
+
+legend({'Enc','In'})
